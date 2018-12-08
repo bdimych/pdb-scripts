4820360584bd44c54168b841181261b0ef182c55
bdimych <bdimych@bdimych-tl1-mageia5>
2018-12-08 18:39:32 -0500

    pdb-3-check-freenet-uploads.sh in progress

--- pdb-3-check-freenet-uploads.sh
+++ pdb-3-check-freenet-uploads.sh
@@ -14,22 +14,26 @@ export SSHPASS
 session=$(date +%s)
 log check-freenet-uploads session $session
 shopt -s lastpipe
-declare -A pfiles
+declare -A pfiles statistics
+
+# get current uploads information: {{{
 $vps_ssh_command $vps_ssh_connection_string find "'$vps_uploads_dir'" -type f -name "'*.7z*'" | while read f
 do
+	let statistics[uploads-files-count]+=1
 	echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-	log file "$f"
+	log file: "$f"
 	f="${f##*/}"
 	name_md5=$(md5sum <<<"$f" | tr -d ' -')
-	log name_md5 $name_md5
+	log name_md5: $name_md5
 	[[ "$f" =~ ^(.+)\.7z ]]
 	base="${BASH_REMATCH[1]}"
 	pf=$(find "$local_packages_dir" -name "$base-in-progress.txt")
 	[[ $pf ]] || error could not find progress file
-	log progress file "$pf"
+	log progress file: "$pf"
 	pfiles["$pf"]=1
 	echo "--------------------------------------------------
-$(mydate) check-freenet-uploads session $session:" >>"$pf"
+$(mydate) check-freenet-uploads session $session:
+File: $f" >>"$pf"
 	echo "
 set -e
 exec 3<>/dev/tcp/127.0.0.1/9481
@@ -39,20 +43,79 @@ while [[ 1 ]]; do read -u3 -t1 x || break; echo \"\$x\"; done
 echo -e 'Disconnect\nEndMessage' >&3
 echo
 " | tee -a "$pf" | $vps_ssh_command $vps_ssh_connection_string >>"$pf" 2>&1
+	echo
 done
 echo
+# }}}
 
+declare -i parts_count parts_done
+declare -A part_progress
+declare errors chk
 for pf in "${!pfiles[@]}"
 do
+	echo '##################################################'
 	echo "${pf##*/}"
-	status=freenet-upload$(sed -n "/check-freenet-uploads session $session/,\$p" "$pf" | perl -ne '
-		if (/check-freenet-uploads session/) {$status{parts}++}
-		elsif (/Started=true/) {$status{started}++}
-		elsif (/Fatal=true/) {$status{fatal}++}
-		elsif (/URI=CHK@.{43},.{43},AAMC--8/) {$status{chk}++}
-		elsif (/error/i && ! /^\w*Filename=/) {$status{error}++}
-		END {for $k (qw(parts started chk error fatal)) {print "-$k($status{$k})" if $status{$k}}}
-	')
-	log package status: $status'\n' | tee -a "$pf"
+	parts_count=0
+	parts_done=0
+	cat "$pf" | while read -r x
+	do
+		if [[ "$x" =~ "check-freenet-uploads session $session" ]]; then
+			parts_count+=1
+			echo -------------------- part $parts_count: ---------------------
+			part_progress=()
+			errors=
+			chk=
+
+		elif [[ $parts_count == 0 ]]; then
+			continue
+
+		elif [[ "$x" =~ File:\ (.+) ]]; then
+			f="${BASH_REMATCH[1]}"
+			echo file: "$f"
+
+		elif [[ "$x" == 'CodeDescription=No such identifier' ]]; then
+			let statistics[not-yet-added]+=1
+			echo not yet added to uploads
+
+		elif [[ "$x" == 'Started=true' ]]; then
+			let statistics[started]+=1
+			echo upload started
+
+		elif [[ "$x" == 'Fatal=true' ]]; then
+			let statistics[fatal]+=1
+			echo upload fatal error
+
+		elif perl -ne '!/^\w*Filename=/ && /error/i || exit 1' <<<"$x"; then
+			errors=1
+			let statistics[errors]+=1
+			echo upload error found: "$x"
+
+		elif [[ "$x" =~ URI=(CHK@.{43},.{43},AAMC--8) && ! $chk ]]; then
+			chk="${BASH_REMATCH[1]}"
+			# TODO: get ssh md5sum and add file to files.txt
+			let statistics[chk]+=1
+			echo chk found
+
+		elif [[ "$x" =~ ^(DataLength|Succeeded|Total|LastProgress)=(.+) ]]; then
+			part_progress[${BASH_REMATCH[1]}]=${BASH_REMATCH[2]}
+
+		elif [[ "$x" == PutSuccessful ]]; then
+			# TODO: ssh mv from uploads dir to completed dir
+			let statistics[upload-done]+=1
+			parts_done+=1
+			echo upload done
+
+		fi
+	done
+	if [[ $parts_count == $parts_done ]]
+	then
+		:
+		# TODO: ??? what to do if all parts done ???
+	fi
+	# TODO: calculate progress
+	# TODO: print status to $pf
+	echo
 done
+
+declare -p statistics
 
