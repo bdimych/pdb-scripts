25465c74d4a2ed8e4603145c597beaf870ad4e6e
bdimych <bdimych@bdimych-tl1-mageia5>
2019-01-02 14:23:25 -0500

    install.sh initial added

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p 'install local or vps part (l|v)? ' which_part
+
+if [[ $which_part == l ]]
+then
+	# install local part of scripts: {{{
+	set -x
+	sudo ln -sfv -t /usr/local/bin "$PWD"/pdb-{1..4}*
+	# }}}
+
+elif [[ $which_part == v ]]
+then
+	# install vps part of scripts: {{{
+	:
+	# }}}
+fi
+
