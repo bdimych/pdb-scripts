5631125cc816d0c3b16af168f2562ddaf79eae9b
bdimych <bdimych@bdimych-tl1-mageia5>
2019-01-06 09:33:41 -0500

    pdb-4-list-statuses.sh minimal functionality

--- pdb-4-list-statuses.sh
+++ pdb-4-list-statuses.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-# pdb-4-list-statuses.sh
+# TODO: description,
+
+source "$(dirname "$(realpath "$0")")/pdb-config.sh"
+source "$(dirname "$(realpath "$0")")/pdb-lib.sh"
+
+find "$local_packages_dir" -name '*in-progress.txt*' | while read pf
+do
+	f="${pf%-in-progress.txt*}.7z"
+	[[ -e "$f" ]] && size="$(( $(stat -c%s "$f")/1024/1024 )) Mb" || size='7z not found'
+	echo "$(grep 'package status' "$pf" | tail -n1 | sed 's/LOG: package status: //'): <${pf##*/}> ($size)"
+done | sort -n | while read x
+do
+	echo "$x"
+	if [[ "$x" =~ freenet-upload-(started-chk-done)+ ]]
+	then
+		echo ready to save
+		# TODO: ssh grep md5 completed.txt
+	fi
+done
 
