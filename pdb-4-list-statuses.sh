f91dc7b9b58d803ee44d886af48a8a837b5b796e
bdimych <bdimych@bdimych-tl1-mageia5>
2019-02-04 16:21:30 -0500

    little cosmetic

--- pdb-4-list-statuses.sh
+++ pdb-4-list-statuses.sh
@@ -8,15 +8,20 @@ source "$(dirname "$(realpath "$0")")/pdb-lib.sh"
 find "$local_packages_dir" -name '*in-progress.txt*' | while read pf
 do
 	f="${pf%-in-progress.txt*}.7z"
-	[[ -e "$f" ]] && size="$(( $(stat -c%s "$f")/1024/1024 )) Mb" || size='7z not found'
-	echo "$(grep 'package status' "$pf" | tail -n1 | sed 's/LOG: package status: //'): <${pf##*/}> ($size)"
+	[[ -e "$f" ]] && size="7z size $(( $(stat -c%s "$f")/1024/1024 )) Mb" || size='7z not found'
+	echo "$(grep 'package status' "$pf" | tail -n1 | sed 's/LOG: package status: //'): ${pf##*/} ($size)"
 done | sort -n | while read x
 do
-	echo "$x"
-	if [[ "$x" =~ freenet-upload-(started-chk-done)+ ]]
+	if [[ "$x" =~ freenet-upload(-started-chk-done)+:\ (.*)\ \( ]]
 	then
-		echo ready to save
-		# TODO: ssh grep md5 completed.txt
+		cat <<eof
++----------
+| $x
+| freenet upload done and now you can run: pdb-5-save-message-to-blockchain.sh $(printf %q "${BASH_REMATCH[2]}")
++----------
+eof
+	else
+		echo "$x"
 	fi
 done
 
